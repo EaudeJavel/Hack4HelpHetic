b04cc7d47a2a3ed47299c121c13639d7d1f2ab0b
EaudeJavel <p.plamy6@gmail.com>
2019-12-17 18:31:32 +0100

    add block video responsive

--- components/blockVideo/blockVideo.vue
+++ components/blockVideo/blockVideo.vue
@@ -1,26 +1,26 @@
 <template>
   <div class="bg">
-    <div class="container">
-      <div class="container__text">
-        <div class="container__text__alias">Édition 2019</div>
-        <h1 class="container__text__title">Créer une solution utilisant le numérique pour que la société intègre mieux les personnes sans-abris</h1>
-        <ul class="container__text__list">
-          <li class="container__text__list__item">
+    <div class="container-block-video">
+      <div class="container-block-video__text">
+        <div class="container-block-video__text__alias">Édition 2019</div>
+        <h1 class="container-block-video__text__title">Créer une solution utilisant le numérique pour que la société intègre mieux les personnes sans-abris</h1>
+        <ul class="container-block-video__text__list">
+          <li class="container-block-video__text__list__item">
             <img src="~/assets/images/puce.svg" alt="">
             2 promotions concernées, soit 180 étudiants
           </li>
-          <li class="container__text__list__item">
+          <li class="container-block-video__text__list__item">
             <img src="~/assets/images/puce.svg" alt="">
             2 semaines dédiées du 9 au 20 décembre 2019 au sein du campus HETIC
           </li>
-          <li class="container__text__list__item">
+          <li class="container-block-video__text__list__item">
             <img src="~/assets/images/puce.svg" alt="">
             Un travail en collaboration avec l'écosystème de la tech (entrepreneurs et experts métiers engagés)
           </li>
         </ul>
-        <cta type="large" link="/more" label="Découvrir l'édition 2019" class="container__text__cta"></cta >
+        <cta type="large" link="/more" label="Découvrir l'édition 2019" class="container-block-video__text__cta"></cta >
       </div>
-        <div class="container__video">
+        <div class="container-block-video__video">
           <video src="~/assets/videos/video.mp4" controls muted autoplay type="video/mp4"></video>
         </div>
     </div>
@@ -35,9 +35,13 @@
   display: flex;
   justify-content: center;
   align-items: center;
+
+  @include tablet {
+    height: 100%;
+  }
 }
 
-.container {
+.container-block-video {
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -46,12 +50,27 @@
   margin-left: 40px;
   margin-right: 40px;
 
+  @include tablet {
+    justify-content: initial;
+    align-items: initial;
+    flex-wrap: wrap;
+    height: 100%;    
+    margin: 40px;
+  }
+
     &__text {
       width: 45%;
       transform: translateX(20px);
 
+      @include tablet {
+        width: 100%;
+        padding: 20px;
+        transform: translateX(0px);
+      }
+
       &__alias {
         text-transform: uppercase;
+        font-weight: bold;
         color: #FF7A00;
         font-family: "metropolis", arial;
       }
@@ -82,10 +101,20 @@
 
     &__video {
       width: 47%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      @include tablet {
+        width: 100%;
+      }
 
       video {
         width: 97%;
-        height: 400px;
+
+        @include tablet {
+          width: 92%;
+        }
       }
     }
 }
@@ -103,14 +132,6 @@
     components: {
       cta
     },
-		computed: {
-			...mapGetters({
-				getTexts: "text/getTexts"
-			}),
-			texts() {
-				return this.getTexts
-			},
-		}
 	}
 </script>
 
