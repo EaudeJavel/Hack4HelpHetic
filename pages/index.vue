7d2c93fbf4883bb04c897cd408a6b5c3e5eebc87
leach <lea.charrier@hetic.net>
2019-12-18 10:11:24 +0100

    banner update

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
 	<div v-if="Object.keys(texts).length" class="container-page container-index">
 		<navBar />
-		<banner />								
+		<banner />
 		<blockText :title="texts.BLOCKTEXT_HOME_TITLE" :text="texts.BLOCKTEXT_HOME_TEXT" :image="texts.BLOCKTEXT_HOME_IMG" />
 		<coverImg :title="texts.COVERIMG_HOME_TITLE" :text="texts.COVERIMG_HOME_TEXT" />
 		<blockVideo />
-		<brands />
+		<!-- <brands /> -->
 		<footerDefault />
 	</div>
 </template>
